625e2879ea3cc5d82e6e334d141f75b5352cdf4f
Galip Berk Ozer <galipberkozer@gmail.com>
2020-03-04 20:47:40 +0000

    Create table properties

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,8 +1,28 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS properties;
 
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
   email VARCHAR(255),
   password VARCHAR(255)
+);
+
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL,
+  title VARCHAR(255),
+  description TEXT,
+  thumbnail_photo_url VARCHAR(255),
+  cover_photo_url VARCHAR(255),
+  cost_per_night INTEGER,
+  parking_spaces SMALLINT,
+  number_of_bathrooms SMALLINT,
+  number_of_bedrooms SMALLINT,
+  country VARCHAR(255),
+  street VARCHAR(255),
+  city VARCHAR(255),
+  province VARCHAR(255),
+  post_code VARCHAR(255),
+  active BOOLEAN
 );
