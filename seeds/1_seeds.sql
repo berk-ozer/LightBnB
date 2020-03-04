c24118e8d9a5afb880ef36fda00b761ecb9adc9f
Galip Berk Ozer <galipberkozer@gmail.com>
2020-03-04 21:24:37 +0000

    Write data to insert into users

---
+++ seeds/1_seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (name, email password) VALUES 
+('Jack Dunfield', 'jackdun@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Katie Moss', 'katiemossz@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Phil Kess', 'philk@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Joe Viro', 'joeviro32@yahoo.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Sally Kessel', 'sallypants@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Luke Bewaj', 'lukecool@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
