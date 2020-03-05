936f3b42375395479e6e1f64169b20d768ae841e
Galip Berk Ozer <galipberkozer@gmail.com>
2020-03-05 14:23:43 +0000

    Connect to database using node-postgres

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,6 +1,10 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
+const { Pool } = require('pg');
+
+const pool = new Pool();
+
 /// Users
 
 /**
